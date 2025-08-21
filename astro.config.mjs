2c0c4559f034bc1a573248e315dd145f33115be5
Jim Diroff II <jim@dirofftech.com>
2025-08-21 02:27:22 -0400

    Completed Astro blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import netlify from "@astrojs/netlify";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jimdiroffii-dot-com.netlify.app/",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   adapter: netlify(),
+  integrations: [preact()],
 });
