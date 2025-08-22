2c17311258d102d8d998b4f7c65c15f430291646
Jim Diroff II <jim@dirofftech.com>
2025-08-21 23:13:26 -0400

    Fix: Astro blog image rendering and schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,15 +3,13 @@ import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/[^_]*.md", base: "./src/blog" }),
-  schema: z.object({
+  schema: ({ image }) => z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
     author: z.string(),
-    image: z.object({
-      url: z.string(),
-      alt: z.string(),
-    }),
+    image: z.union([image(), z.string().url()]),
+    imageAlt: z.string(),
     tags: z.array(z.string()),
   }),
 });
