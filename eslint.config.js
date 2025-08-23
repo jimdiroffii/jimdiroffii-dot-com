9bc27e2759691543c7319c0f600b995790967e97
Jim Diroff II <jim@dirofftech.com>
2025-08-23 00:07:11 -0400

    conf: add tsx, jsx and a11y to lint script

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+
 export default [
-  // add more generic rule sets here, such as:
-  // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
   {
-    rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
-    },
+    files: ["**/*.{jsx,tsx,astro}"],
+    plugins: { "jsx-a11y": jsxA11y },
+    rules: { ...jsxA11y.configs.recommended.rules },
+  },
+  {
+    ignores: ["dist/**", ".netlify/**"],
   },
 ];
