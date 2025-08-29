86d32aad0172a8eadbf36357bb1f0daa016e38d4
Jim Diroff II <jim@dirofftech.com>
2025-08-29 01:32:11 -0400

    feat: working on markdown layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,8 @@ import netlify from '@astrojs/netlify';
 
 import svelte from '@astrojs/svelte';
 
-// https://astro.build/config
 export default defineConfig({
-  // TODO: Update Domain
-  site: 'https://jimdiroffii-dot-com.netlify.app/',
+  site: 'https://www.jimdiroffii.com/',
 
   vite: {
     plugins: [tailwindcss()]
